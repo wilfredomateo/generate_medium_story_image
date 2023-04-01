e17d872b57f06c314a6fd9a862e2aa007c28932d
wilfredomateo <wmateo86@gmail.com>
2023-04-01 15:38:28 -0500

    edits to generate

--- generate_AI_image.py
+++ generate_AI_image.py
@@ -7,7 +7,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 #Generate keywords
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Extract keywords from this text:\n\nBlack-on-black ware is a 20th- and 21st-century pottery tradition developed by the Puebloan Native American ceramic artists in Northern New Mexico. Traditional reduction-fired blackware has been made for centuries by pueblo artists. Black-on-black ware of the past century is produced with a smooth surface, with the designs applied through selective burnishing or the application of refractory slip. Another style involves carving or incising designs and selectively polishing the raised areas. For generations several families from Kha'po Owingeh and P'ohwhóge Owingeh pueblos have been making black-on-black ware with the techniques passed down from matriarch potters. Artists from other pueblos have also produced black-on-black ware. Several contemporary artists have created works honoring the pottery of their ancestors.",
+  prompt="",
   temperature=0.5,
   max_tokens=60,
   top_p=1.0,
